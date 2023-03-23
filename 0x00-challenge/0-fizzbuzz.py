b18aa59c64f7c2af21aadcd8ff9a04e0c2cc9056
isgameliel <simvicpro@gmail.com>
2023-03-23 02:17:35 +0100

    error fixed

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" FizzBuzz
+""" 
+FizzBuzz
 """
 import sys
 
@@ -16,11 +17,11 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if i % 3 == 0 and i % 5 == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
+        elif i % 3 == 0:
+            tmp_result.append("Fizz")
+        elif i % 5 == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
@@ -36,3 +37,4 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
+    
